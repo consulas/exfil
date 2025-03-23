66b9dad949af3f023dfd63da9a047fba8bb923dd
Michael Huang <consulas753@gmail.com>
2025-03-23 02:09:01 -0400

    Added absolute paths to apply script, added gitignore

--- apply_patch.py
+++ apply_patch.py
@@ -16,29 +16,33 @@ def main():
     parser = argparse.ArgumentParser(
         description="Apply a git patch to re-create the repository in the destination folder."
     )
-    parser.add_argument("patch", help="Path to the git patch file")
-    parser.add_argument("destination", help="Destination folder where the patch will be applied")
+    parser.add_argument("patchfile", help="Path to the git patch file")
+    parser.add_argument("destination_folder", help="Destination folder where the patch will be applied")
     args = parser.parse_args()
 
+    # Get absolute paths for patch file and destination folder
+    patchfile = os.path.abspath(args.patchfile)
+    destination_folder = os.path.abspath(args.destination_folder)
+
     # Check if patch file exists.
-    if not os.path.isfile(args.patch):
+    if not os.path.isfile(patchfile):
         sys.exit("Error: Patch file does not exist.")
 
     # Create destination folder if it doesn't exist.
-    if not os.path.exists(args.destination):
-        os.makedirs(args.destination)
+    if not os.path.exists(destination_folder):
+        os.makedirs(destination_folder)
 
-    # Change working directory to destination.
-    os.chdir(args.destination)
+    # Change working directory to destination folder.
+    os.chdir(destination_folder)
 
     # Initialize a new git repository if one doesn't exist.
-    if not os.path.isdir(os.path.join(args.destination, ".git")):
+    if not os.path.isdir(os.path.join(destination_folder, ".git")):
         init_result = subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
         if init_result.returncode != 0:
             sys.exit(f"Error initializing git repository: {init_result.stderr.strip()}")
 
     # Apply the patch.
-    apply_cmd = ["git", "apply", "--whitespace=fix", args.patch]
+    apply_cmd = ["git", "apply", "--whitespace=fix", patchfile]
     apply_result = subprocess.run(apply_cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     if apply_result.returncode != 0:
         sys.exit(f"Error applying patch: {apply_result.stderr.strip()}")
